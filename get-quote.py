0b135a6606db42a760bda30419ebeca1b1238e26
srps <a_k_a_bouncer@hotmail.com>
2022-04-01 02:09:11 +0100

    Added quote number parameter, removed newline after last quote

--- get-quote.py
+++ get-quote.py
@@ -1,14 +1,15 @@
 import random
+import sys
 
 
-def run():
+def run(num_quotes=1):
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
 
   last = len(quotes) - 1
-  rnd = random.randint(0, last)
-  print(quotes[rnd])
+  for i in range(int(num_quotes)):
+    print(quotes[random.randint(0, last)], end="")
 
 if __name__== "__main__":
-  run()
+  run(*sys.argv[1:])
